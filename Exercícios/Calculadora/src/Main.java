cd5075a7051f0b2203824ad23d07ea258ea3ff8b
Matheus Renó Torres <Matheusrenor@gmail.com>
2024-03-09 18:11:54 -0300

    Calculadora

---
+++ Exercícios/Calculadora/src/Main.java
@@ -0,0 +1,11 @@
+import Models.Calculadora;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculadora.circulferencia(71));
+        System.out.println(Calculadora.volumeEsfera(1));
+        System.out.println(Calculadora.volumeCilindro(12.3,3.25));
+    }
+}
